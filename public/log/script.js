1dfeee9af0e5dc3c951ac255c85d020149b27c1a
Divyam Jain <jaindivyam111@gmail.com>
2022-06-06 21:25:27 +0530

    updated

--- public/log/script.js
+++ public/log/script.js
@@ -1,21 +1,32 @@
+
+const map=L.map('map').setView([0,0],1);
+const attribution =
+'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tiles=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+attribution
+});
+tiles.addTo(map);
+
+
 getData();
 async function getData(){
     const response=await fetch('/api');
     const data=await response.json();
 
     for(item of data){
-        const root=document.createElement('p');
-        const geo=document.createElement('div');
-        const date=document.createElement('div');
-        const temp=document.createElement('div');
-        const weather=document.createElement('div');
-        const dateString=new Date(item.timestamp).toDateString();
-        date.textContent=dateString;
-        weather.textContent=`weather: ${item.weather}`;
-        temp.textContent=`min_temp: ${item.temp_min},max_temp: ${item.temp_max}`
-        geo.textContent=`${item.lat},${item.lon}`;
-        root.append(geo,date,temp,weather);
-        document.body.append(root);
+        L.marker([item.lat,item.lon]).addTo(map);
+        // const root=document.createElement('p');
+        // const geo=document.createElement('div');
+        // const date=document.createElement('div');
+        // const temp=document.createElement('div');
+        // const weather=document.createElement('div');
+        // const dateString=new Date(item.timestamp).toDateString();
+        // date.textContent=dateString;
+        // weather.textContent=`weather: ${item.weather}`;
+        // temp.textContent=`min_temp: ${item.temp},max_temp: ${item.temp_max}`
+        // geo.textContent=`${item.lat},${item.lon}`;
+        // root.append(geo,date,temp,weather);
+        // document.body.append(root);
     }
     console.log(data);
 }
--- public/script/script.js
+++ public/script/script.js
@@ -1,12 +1,22 @@
 getGeo();
 function getGeo(){
-
-   if('geolocation' in navigator){
+    
+    
+    
+    if('geolocation' in navigator){
         console.log('geolocation available');
         navigator.geolocation.getCurrentPosition(async position=>{
-        let lat=position.coords.latitude;
-        let lon = position.coords.longitude;
-        let btn=document.getElementById('send');
+            let lat=position.coords.latitude;
+            let lon = position.coords.longitude;
+            const map=L.map('map').setView([lat,lon],3);
+            const attribution =
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+        const tiles=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+            attribution
+        });
+        tiles.addTo(map);
+        let mkr=L.marker([lat,lon]).addTo(map);
+        // let btn=document.getElementById('send');
         document.getElementById('latitude').innerText=lat;
         document.getElementById('longitude').innerText=lon;
         const api_url=`/weather/${lat},${lon}`;
@@ -15,17 +25,18 @@ function getGeo(){
         const json=await response.json();
         console.log(json);
         let temp_max=json.main.temp_max;
-        let temp_min=json.main.temp_min;
+        let temp=json.main.temp;
         let weather=json.weather[0].description;
+        document.getElementById('condition').textContent=weather;
+        document.getElementById('temp').textContent=temp;
+
         
         
-        document.getElementById('send');
+        // document.getElementById('send');
         
         
         
-        
-        send.addEventListener("click",async()=>{
-        const data={lat,lon,temp_max,temp_min,weather};
+        const data={lat,lon,temp_max,temp,weather};
 
         const option={
             method:'POST',
@@ -34,13 +45,12 @@ function getGeo(){
             },
             body:JSON.stringify(data)
         };
-        const response=await fetch('/api',option);
-        const json=await response.json();
-        console.log(json);
+        const fetch_response=await fetch('/api',option);
+        const fetch_json=await fetch_response.json();
+        console.log(fetch_json);
             
         });
         
-        });
     }
     else{
         console.log('geolocation not available');
